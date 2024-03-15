df33ca8100ced46a0b9879e11469441872d5dc86
corni <gmigan.a@gmail.com>
2024-03-16 00:03:10 +0100

    feat: create mobile layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,36 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'desktop-day': "url('../public/starter-code/assets/desktop/bg-image-daytime.jpg')",
+        'desktop-night': "url('../public/starter-code/assets/desktop/bg-image-nighttime.jpg')",
+      },
+      backgroundColor: {
+        "dark": "#303030",
+        "gray": "#D8D8D8"
+      },
+      textColor: {
+        "dark": "#303030"
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"]
+      },
+      fontSize: {
+        sm: '0.8rem',
+        base: '1.2rem',
+        mb: '1.5rem',
+        xl: '1.8rem',
+        'h1': '20rem',
+        'h2': '5.6rem',
+        'h3': '2.4rem',
+        'h4': '2rem',
+      },
+      letterSpacing: {
+        h6: "3px",
+        button: "3.75px"
+      },
+    },
   },
   plugins: [],
 }
