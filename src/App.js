f5a5c8647d051fe7f4cc4f8cee37816e6d78d5c6
corni <gmigan.a@gmail.com>
2024-03-16 15:16:43 +0100

    feat: add the possibility to switch between a dark and light theme

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, createContext } from 'react';
 import Switch from './Switch';
 import Panel from './Panel';
 import Clock from './Clock';
@@ -6,19 +6,23 @@ import Quote from './Quote';
 
 import './App.css';
 
+export const ThemeContext = createContext(null);
+
 function App() {
   const [isOpen, setIsOpen] = useState(false);
-
+  const  [isDayTime, setIsDayTime] = useState(false)
   return (
     <>
     <main className='h-screen bg-black relative opacity-99'>
-      <img className="h-screen object-cover opacity-60" src={require("./assets/desktop/bg-image-daytime.jpg")} alt="Day time" />
-      <article className='grid grid-cols-10 grid-rows-8 absolute top-0 w-full h-full text-white font-normal py-11 px-8'>
-        <Quote className={"col-start-1 col-end-11 row-start-1 row-end-5"} open={{isOpen, setIsOpen}} />
-        <Clock open={{isOpen, setIsOpen}}/>
-        <Switch className={`${isOpen ? "col-start-1 col-end-11 row-start-5 row-end-9" : "col-start-1 col-end-11 row-start-8 row-end-9"}`} data={{isOpen, setIsOpen}}/>
-      </article>
-      <Panel open={{isOpen, setIsOpen}} />
+      <ThemeContext.Provider value={isDayTime}>
+        <img className="h-screen object-cover opacity-60" src={isDayTime ? require("./assets/desktop/bg-image-daytime.jpg") : require("./assets/desktop/bg-image-nighttime.jpg")} alt="Day time" />
+        <article className='grid grid-cols-10 grid-rows-8 absolute top-0 w-full h-full text-white font-normal py-11 px-8'>
+          <Quote className={"col-start-1 col-end-11 row-start-1 row-end-5"} open={{isOpen, setIsOpen}} />
+          <Clock open={{isOpen, setIsOpen}}/>
+          <Switch className={`${isOpen ? "col-start-1 col-end-11 row-start-5 row-end-9" : "col-start-1 col-end-11 row-start-8 row-end-9"}`} data={{isOpen, setIsOpen}}/>
+        </article>
+        <Panel open={{isOpen, setIsOpen}} />
+      </ThemeContext.Provider>
     </main>
     </>
   );
--- src/Panel.js
+++ src/Panel.js
@@ -1,22 +1,25 @@
-function Panel({open, className}) {
+import { useContext } from 'react';
+import { ThemeContext } from './App';
 
+function Panel({open, className}) {
+    const dayTheme = useContext(ThemeContext);
     return (
       <>
-        <aside className={`opacity-90 bg-white h-[40vh] w-full absolute top-full -translate-y-full ${open.isOpen || "hidden"} ${className}`}>
+        <aside className={`opacity-90 ${dayTheme ? "bg-white text-dark" : "bg-black text-white"}  h-[40vh] w-full absolute top-full -translate-y-full ${open.isOpen || "hidden"} ${className}`}>
             <div className='mx-10 my-16 grid grid-cols-2 grid-rows-4 items-center h-[calc(40vh-_8rem)]'>
                 <p className='uppercase tracking-mb col-span-1 row-span-1 '>CURRENT TIMEZONE</p>
-                <p className='font-bold text-dark text-h4 col-start-2 col-end-3 row-span-1 text-right'>Europe/London</p>
+                <p className='font-bold  text-h4 col-start-2 col-end-3 row-span-1 text-right'>Europe/London</p>
 
 
                 <p className='uppercase tracking-mb col-span-1'>Day of the year</p>
-                <p className='font-bold text-dark text-h4 col-start-2 col-end-3 text-right'>295</p>
+                <p className='font-bold text-h4 col-start-2 col-end-3 text-right'>295</p>
 
                 <p className='uppercase tracking-mb col-span-1'>day of the week</p>
-                <p className='font-bold text-dark text-h4 col-start-2 col-end-3 text-right'>5</p>
+                <p className='font-bold text-h4 col-start-2 col-end-3 text-right'>5</p>
 
 
                 <p className='uppercase tracking-mb col-span-1'>week number</p>
-                <p className='font-bold text-dark text-h4 col-start-2 col-end-3 text-right'>42</p>
+                <p className='font-bold text-h4 col-start-2 col-end-3 text-right'>42</p>
             </div>
         </aside>
       </>
